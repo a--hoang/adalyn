4787cb91ae816d91b5b2477a9c47f5bb928b33c1
Andrew Hoang <ahoang18@bu.edu>
2017-04-02 14:57:02 -0400

    Spawn child without timeout

--- src/motor.js
+++ src/motor.js
@@ -2,28 +2,16 @@ var spawn = require('child_process').spawn
 
 module.exports.left = function () {
   var child = spawn('python', ['./left.py'])
-  setTimeout(function () {
-    child.kill('SIGINT')
-  }, 400)
 }
 
 module.exports.up = function () {
   var child = spawn('python', ['./forward.py'])
-  setTimeout(function () {
-    child.kill('SIGINT')
-  }, 400)
 }
 
 module.exports.down = function () {
   var child = spawn('python', ['./down.py'])
-  setTimeout(function () {
-    child.kill('SIGINT')
-  }, 400)
 }
 
 module.exports.right = function () {
   var child = spawn('python', ['./right.py'])
-  setTimeout(function () {
-    child.kill('SIGINT')
-  }, 400)
 }
